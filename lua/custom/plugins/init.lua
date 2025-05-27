478abfb73190c8a8dc1228ce76393db13b90bb01
ShravanCool <shravan.akku@gmail.com>
2025-05-27 13:00:16 +0530

    Added new custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'scrooloose/nerdcommenter',
+  {
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'G', 'Gdiffsplit', 'Gvdiffsplit', 'Gwrite', 'Gread', 'Gedit' }, -- optional lazy-loading
+    keys = {
+      { '<leader>gs', ':Git<CR>', desc = '[G]it [S]tatus (Fugitive)' },
+      { '<leader>gc', ':Git commit<CR>', desc = '[G]it [C]ommit' },
+      { '<leader>gp', ':Git push<CR>', desc = '[G]it [P]ush' },
+      { '<leader>gl', ':Git pull<CR>', desc = '[G]it Pu[l]l' },
+      { '<leader>gb', ':Git blame<CR>', desc = '[G]it [B]lame (Fugitive)' },
+    },
+  },
+}
